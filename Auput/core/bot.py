771c30c8212f4c6aa3ccf956c91619362ddb35e3
Hacktivist Of Garuda <164336475+apasihco@users.noreply.github.com>
2024-03-29 04:00:14 +0700

    Update bot.py

--- Auput/core/bot.py
+++ Auput/core/bot.py
@@ -13,7 +13,7 @@ class AuputBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "NezukoMusicBot",
+            "Titansx_bot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
