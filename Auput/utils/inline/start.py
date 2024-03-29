497e11d66efb5201c3374ff5557b2902ac6de80a
Hacktivist Of Garuda <164336475+apasihco@users.noreply.github.com>
2024-03-29 10:48:03 +0700

    Update start.py

--- Auput/utils/inline/start.py
+++ Auput/utils/inline/start.py
@@ -45,9 +45,10 @@ def start_pannel(_):
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
-            InlineKeyboardButton(text="🔎 Menu 🔍", callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="Menu", callback_data="settings_back_helper"),
             InlineKeyboardButton(text="CH", url=f"https://t.me/mylifetitans"),
             InlineKeyboardButton(text="OWNER", url="https://t.me/itsmetitans"),
+            InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG"),
         ]
     ]
     buttons.append(
@@ -56,11 +57,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 text=_["S_B_5"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
-        ],
-        [
-            InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG"),
         ]
-        
     )
     if GITHUB_REPO:
         buttons.append(
